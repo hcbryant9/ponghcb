cef381430112c71c62a6b15c56db7c5aec472d1d
Henry Bryant <69921401+hcbryant9@users.noreply.github.com>
2022-10-02 22:34:44 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -3,14 +3,16 @@ import random
 from flask import Flask, jsonify, request 
 from flask_httpauth import HTTPDigestAuth as FlaskDigestAuth
 
+
+#https://ponghcb.herokuapp.com/
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key here'
 auth = FlaskDigestAuth()
-if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000,debug=True)
-tester = random.randint(1,10)
+num = random.randint(1,100)
 
 users = {
     "vcu":"rams"
@@ -22,12 +24,13 @@ def get_pw(username):
         return users.get(username)
     return None
 
-class ValidationError(ValueError):
-    pass
 
-@app.route('/users', methods=['GET'])
+
+@app.route('/pong', methods=['GET'])
 @auth.login_required
 def index():
-   return jsonify(tester),201
+   return jsonify(num)
 
 
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=5000,debug=True)
